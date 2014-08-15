369dd98fa043852d47777418f4787b85c239669a
mmalohlava <michal.malohlava@gmail.com>
2014-08-15 15:06:46 -0700

    Delete old jar before making package.

--- h2o-examples/make-package.sh
+++ h2o-examples/make-package.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+rm target/*.jar
 mvn package -DXX:MaxPermSize=128m -DskipTests -Dclean.skip -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dscalastyle.skip=true -Dmaven.scaladoc.skip=true -Dskip=true
