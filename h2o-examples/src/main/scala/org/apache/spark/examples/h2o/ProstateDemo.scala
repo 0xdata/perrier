dac8c81592fd06bb5885ad9a5d2f7fa538c8bc8a
mmalohlava <michal.malohlava@gmail.com>
2014-08-20 17:58:25 -0700

    Extracting SchemaUtils to shared them among demos.

--- h2o-examples/src/main/scala/org/apache/spark/examples/h2o/ProstateDemo.scala
+++ h2o-examples/src/main/scala/org/apache/spark/examples/h2o/ProstateDemo.scala
@@ -89,39 +89,5 @@ object ProstateParse extends Serializable {
   }
 }
 
-/** Simple support for parsing data. */
-object SchemaUtils {
 
-  def int  (s:String):Option[Int]     = if (isValid(s)) parseInt(s)   else None
-  def float(s:String):Option[Float]   = if (isValid(s)) parseFloat(s) else None
-  def str  (s:String):Option[String]  = if (isValid(s)) Option(s)     else None
-  def bool (s:String):Option[Boolean] = if (isValid(s)) parseBool(s)  else None
-
-  def parseInt(s:String):Option[Int] =
-    try { Option(s.trim().toInt) } catch {
-      case e:NumberFormatException => None
-    }
-
-  def parseFloat(s:String):Option[Float] =
-    try { Option(s.trim().toFloat) } catch {
-      case e:NumberFormatException => None
-    }
-
-  def parseBool(s:String):Option[Boolean] = s.trim().toLowerCase match {
-    case "true"|"yes" => Option(true)
-    case "false"|"no" => Option(false)
-    case _ => None
-  }
-
-  def isNA(s:String) = s==null || s.isEmpty || (s.trim.toLowerCase match {
-    case "na" => true
-    case "n/a"=> true
-    case _ => false
-  })
-
-  def isValid(s:String) = !isNA(s)
-
-  //def na[S]:S = null.asInstanceOf[S]
-  //def na[S]:Option[S] = None
-}
 
