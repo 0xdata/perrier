bbbfa80834d28261425a38df7425a8d7ce5c02aa
Cliff Click <cliffc@acm.org>
2014-08-31 19:10:08 -0700

    Overhaul the perrier functionality
    H2OContext becomes static class, not needed in most places.  Has
    utilities to convert RDD & SchemaRDD -> DataFrame
    H2ORDD only supports DataFrame -> lazy RDD.  No longer converts RDDs ->
    DataFrames

--- h2o-perrier/src/main/scala/org/apache/spark/h2o/H2OContext.scala
+++ h2o-perrier/src/main/scala/org/apache/spark/h2o/H2OContext.scala
@@ -1,10 +1,10 @@
 package org.apache.spark.h2o
 
-import java.io.File
-
 import org.apache.spark.sql.catalyst.types.{BinaryType, DoubleType, IntegerType, DataType, FloatType}
+import java.io.File
+import water.fvec.DataFrame
+import water.Key
 import org.apache.spark.sql.{Row, SchemaRDD}
-
 import scala.language.implicitConversions
 import org.apache.spark.{TaskContext, SparkContext}
 import org.apache.spark.annotation.AlphaComponent
@@ -23,66 +23,114 @@ class H2OContext(@transient val sparkContext: SparkContext)
   with H2OConf
   with Serializable {
 
-  private def perPartition[A: ClassTag]( h2ordd: H2ORDD[A] ) ( context: TaskContext, it: Iterator[A] ): (Int,Long) = {
-    // An array of H2O NewChunks; A place to record all the data in this partition
-    val nchks = water.fvec.Frame.createNewChunks(h2ordd.h2oName,context.partitionId)
-    it.foreach(row => {                    // For all rows...
-      val prod = row.asInstanceOf[Product] // Easy access to all fields
-      for( i <- 0 until prod.productArity ) { // For all fields...
-        val fld = prod.productElement(i)
-        nchks(i).addNum({                  // Copy numeric data from fields to NewChunks
-          val x = fld match { case Some(n) => n; case _ => fld }
-          x match {
-            case n: Number => n.doubleValue
-            case n: Boolean => if (n) 1 else 0
-            case _ => Double.NaN
-          }
-        })
-      }
-    })
-    // Compress & write out the Partition/Chunks
-    water.fvec.Frame.closeNewChunks(nchks)
-    // Return Partition# and rows in this Partition
-    (context.partitionId,nchks(0).len)
+//private def perPartition[A: ClassTag]( h2ordd: H2ORDD[A] ) ( context: TaskContext, it: Iterator[A] ): (Int,Long) = {
+//  // An array of H2O NewChunks; A place to record all the data in this partition
+//  val nchks = water.fvec.Frame.createNewChunks(h2ordd.h2oName,context.partitionId)
+//  it.foreach(row => {                    // For all rows...
+//    val prod = row.asInstanceOf[Product] // Easy access to all fields
+//    for( i <- 0 until prod.productArity ) { // For all fields...
+//      val fld = prod.productElement(i)
+//      nchks(i).addNum({                  // Copy numeric data from fields to NewChunks
+//        val x = fld match { case Some(n) => n; case _ => fld }
+//        x match {
+//          case n: Number => n.doubleValue
+//          case n: Boolean => if (n) 1 else 0
+//          case _ => Double.NaN
+//        }
+//      })
+//    }
+//  })
+//  // Compress & write out the Partition/Chunks
+//  water.fvec.Frame.closeNewChunks(nchks)
+//  // Return Partition# and rows in this Partition
+//  (context.partitionId,nchks(0).len)
+//}
+//
+//private def perSQLPartition( h2ordd: H2ORDD[_], types: Array[Class[_]] ) ( context: TaskContext, it: Iterator[Row] ): (Int,Long) = {
+//  val nchks = water.fvec.Frame.createNewChunks(h2ordd.h2oName,context.partitionId)
+//  it.foreach(row => {
+//    for( i <- 0 until h2ordd.colNames.length) {
+//      nchks(i).addNum(
+//        if (row.isNullAt(i)) Double.NaN
+//        else types(i) match {
+//          case q if q == classOf[Integer] => row.getInt(i)
+//          case q if q == classOf[java.lang.Float] => row.getFloat(i)
+//          case q if q == classOf[java.lang.Double] => row.getDouble(i)
+//          case q if q == classOf[Boolean] => if( row.getBoolean(i)) 1 else 0
+//          case uet => throw new IllegalArgumentException(s"Unexpected type $uet")
+//        }
+//      )
+//    }
+//  })
+//  // Compress & write out the Partition/Chunks
+//  water.fvec.Frame.closeNewChunks(nchks)
+//  // Return Partition# and rows in this Partition
+//  (context.partitionId,nchks(0).len)
+//}
+//
+//private def dataTypeToClass(dt : DataType):Class[_] = dt match {
+//  case BinaryType  => classOf[Integer]
+//  case IntegerType => classOf[Integer]
+//  case FloatType   => classOf[java.lang.Float]
+//  case DoubleType  => classOf[java.lang.Double]
+//  case _ => throw new IllegalArgumentException(s"Unsupported type $dt")
+//}
+//
+//// Returns an H2ORDD as a side-effect makes an H2O Frame with the RDD data.
+//// The input RDD is eagerly evaluated
+//def createH2ORDD[A: TypeTag: ClassTag](rdd: RDD[A], name: String): H2ORDD[A] = {
+//  // Pull out the RDD members; the names become H2O column names; the type guide conversion
+//  val tt = typeOf[A].members.sorted.filter(!_.isMethod).toArray
+//  val names = tt.map(_.name.toString.trim)
+//
+//  // Make an H2ORDD, and within it an H2O data Frame - but with no backing data (yet)
+//  val h2ordd = new H2ORDD(name, names, this, this.sparkContext, rdd, null)
+//  h2ordd.fr.preparePartialFrame(names)
+//
+//  // Parallel-fill the H2O data Chunks
+//  val rows = sparkContext.runJob(h2ordd, perPartition[A](h2ordd) _) // eager, not lazy, evaluation
+//  val res = new Array[Long](rdd.partitions.size)
+//  rows.foreach{ case(cidx,nrows) => res(cidx) = nrows }
+//
+//  // Add Vec headers per-Chunk, and finalize the H2O Frame
+//  h2ordd.fr.finalizePartialFrame(res)
+//  h2ordd
+//}
+//
+//// Returns an H2ORDD referring to an H2O Frame built from the given filename
+//def parse[A: ClassTag]( name : String, f : File ) = {
+//  val fr : water.fvec.Frame = water.util.FrameUtils.parseFrame(water.Key.make(name), f)
+//  new H2ORDD[A](name, fr.names(), this, this.sparkContext, null, fr)
+//}
+}
+
+object H2OContext {
+
+  def colsFromType[A: TypeTag]() : Array[String] = {
+    // Pull out the RDD members; the names become H2O column names; the type guide conversion
+    val tt = typeOf[A].members.sorted.filter(!_.isMethod).toArray
+    tt.map(_.name.toString.trim)
   }
 
-  private def perSQLPartition( h2ordd: H2ORDD[_], types: Array[Class[_]] ) ( context: TaskContext, it: Iterator[Row] ): (Int,Long) = {
-    val nchks = water.fvec.Frame.createNewChunks(h2ordd.h2oName,context.partitionId)
-    it.foreach(row => {
-      for( i <- 0 until h2ordd.colNames.length) {
-        nchks(i).addNum(
-          if (row.isNullAt(i)) Double.NaN
-          else types(i) match {
-            case q if q == classOf[Integer] => row.getInt(i)
-            case q if q == classOf[java.lang.Float] => row.getFloat(i)
-            case q if q == classOf[java.lang.Double] => row.getDouble(i)
-            case q if q == classOf[Boolean] => if( row.getBoolean(i)) 1 else 0
-            case uet => throw new IllegalArgumentException(s"Unexpected type $uet")
-          }
-        )
-      }
-    })
-    // Compress & write out the Partition/Chunks
-    water.fvec.Frame.closeNewChunks(nchks)
-    // Return Partition# and rows in this Partition
-    (context.partitionId,nchks(0).len)
-  }
-
-  // Dedicated path for SchemaRDD which are not generic and represent RDD[Row] (it is kind of C++ template specialization ;-)
-  def createH2ORDD(rdd: SchemaRDD, name: String): H2ORDD[_] = {
+  def toDataFrame(rdd: SchemaRDD) : DataFrame = {
     val names = rdd.schema.fieldNames.toArray
     val types = rdd.schema.fields.map( field => dataTypeToClass(field.dataType) ).toArray
 
-    val h2ordd = new H2ORDD(name, names, this, this.sparkContext, Some(rdd), None)
-    h2ordd.fr.get.preparePartialFrame(names)
+    // Make an H2O data Frame - but with no backing data (yet)
+    val key = Key.rand
+    val fr = new water.fvec.Frame(key)
+    fr.preparePartialFrame(names)
 
-    val rows = sparkContext.runJob(h2ordd, perSQLPartition(h2ordd, types) _) // eager, not lazy, evaluation
-    val res = new Array[Long](rdd.partitions.size)
-    rows.foreach{ case(cidx,nrows) => res(cidx) = nrows }
+    // Extend the SchemaRDD to pass along the frame name
 
-    // Add Vec headers per-Chunk, and finalize the H2O Frame
-    h2ordd.fr.get.finalizePartialFrame(res)
-    h2ordd
+    //val rows = sparkContext.runJob(h2ordd, perSQLPartition(h2ordd, types) _) // eager, not lazy, evaluation
+    //val res = new Array[Long](rdd.partitions.size)
+    //rows.foreach{ case(cidx,nrows) => res(cidx) = nrows }
+    //  
+    //// Add Vec headers per-Chunk, and finalize the H2O Frame
+    //h2ordd.fr.get.finalizePartialFrame(res)
+    //h2ordd
+    ???
   }
 
   private def dataTypeToClass(dt : DataType):Class[_] = dt match {
@@ -93,30 +141,26 @@ class H2OContext(@transient val sparkContext: SparkContext)
     case _ => throw new IllegalArgumentException(s"Unsupported type $dt")
   }
 
-  // Returns an H2ORDD as a side-effect makes an H2O Frame with the RDD data.
-  // The input RDD is eagerly evaluated
-  def createH2ORDD[A: TypeTag: ClassTag](rdd: RDD[A], name: String): H2ORDD[A] = {
-    // Pull out the RDD members; the names become H2O column names; the type guide conversion
-    val tt = typeOf[A].members.sorted.filter(!_.isMethod).toArray
-    val names = tt.map(_.name.toString.trim)
+  //private def perSQLPartition( h2ordd: H2ORDD[_], types: Array[Class[_]] ) ( context: TaskContext, it: Iterator[Row] ): (Int,Long) = {
+  //  val nchks = water.fvec.Frame.createNewChunks(h2ordd.h2oName,context.partitionId)
+  //  it.foreach(row => {
+  //    for( i <- 0 until h2ordd.colNames.length) {
+  //      nchks(i).addNum(
+  //        if (row.isNullAt(i)) Double.NaN
+  //        else types(i) match {
+  //          case q if q == classOf[Integer] => row.getInt(i)
+  //          case q if q == classOf[java.lang.Float] => row.getFloat(i)
+  //          case q if q == classOf[java.lang.Double] => row.getDouble(i)
+  //          case q if q == classOf[Boolean] => if( row.getBoolean(i)) 1 else 0
+  //          case uet => throw new IllegalArgumentException(s"Unexpected type $uet")
+  //        }
+  //      )
+  //    }
+  //  })
+  //  // Compress & write out the Partition/Chunks
+  //  water.fvec.Frame.closeNewChunks(nchks)
+  //  // Return Partition# and rows in this Partition
+  //}
 
-    // Make an H2ORDD, and within it an H2O data Frame - but with no backing data (yet)
-    val h2ordd = new H2ORDD(name, names, this, this.sparkContext, rdd, null)
-    h2ordd.fr.preparePartialFrame(names)
-
-    // Parallel-fill the H2O data Chunks
-    val rows = sparkContext.runJob(h2ordd, perPartition[A](h2ordd) _) // eager, not lazy, evaluation
-    val res = new Array[Long](rdd.partitions.size)
-    rows.foreach{ case(cidx,nrows) => res(cidx) = nrows }
-
-    // Add Vec headers per-Chunk, and finalize the H2O Frame
-    h2ordd.fr.finalizePartialFrame(res)
-    h2ordd
-  }
-
-  // Returns an H2ORDD referring to an H2O Frame built from the given filename
-  def parse[A: ClassTag]( name : String, f : File ) = {
-    val fr : water.fvec.Frame = water.util.FrameUtils.parseFrame(water.Key.make(name), f)
-    new H2ORDD[A](name, fr.names(), this, this.sparkContext, null, fr)
-  }
+  def toRDD[A: TypeTag: ClassTag]( sc : SparkContext, fr : DataFrame ) : RDD[A] = new H2ORDD[A](sc,fr)
 }
--- h2o-perrier/src/main/scala/org/apache/spark/rdd/H2ORDD.scala
+++ h2o-perrier/src/main/scala/org/apache/spark/rdd/H2ORDD.scala
@@ -1,71 +1,53 @@
 package org.apache.spark.rdd
 
+//import org.apache.spark.h2o.H2OContext
+
 import org.apache.spark.h2o.H2OContext
 import org.apache.spark.{TaskContext, Partition, SparkContext}
 import water.fvec.DataFrame
 import water.{DKV, Key}
 import scala.reflect.ClassTag
+import scala.reflect.runtime.universe._
 
 /**
- * Testing support for H2O lazy RDD loading data from H2O KV-store.
- *
- * It should contain only RDD-related implementation, all support methods
- * should be offloaded into H2ORDDLike trait.
- *
- * NOTES:
- *  - spark context to contain h2o info
- *  - sparkcontext.initH2O() returns H2OContext
- *  - import H2OContext
- *  - registerRDDasFrame(RDD, "frame reference")
- *
- *  TODO: create JIRA support arbitrary spark runtime extension, arbitrary extension data
- *
+ * Convert DataFrame into an RDD (lazily)
  */
 
-...
 private[spark]
-class H2ORDD[A: ClassTag](val h2oName: String,
-             val colNames: Array[String],
-             @transient val hc: H2OContext,
-             @transient sc: SparkContext,
-             @transient rdd: Option[RDD[A]],
-             @transient fr:  Option[water.fvec.DataFrame])
-  extends RDD[A](sc, Nil) {
-    if( rdd.isEmpty && fr.isEmpty ) throw new IllegalArgumentException("Must pass in either an RDD or a DataFrame")
+class H2ORDD[A: TypeTag: ClassTag] private (@transient sc: SparkContext, @transient fr: DataFrame, val colNames: Array[String]) extends RDD[A](sc, Nil) {
+
+  // Get column names before building an RDD
+  def this(sc: SparkContext, fr : DataFrame ) = this(sc,fr,H2OContext.colsFromType[A])
+
+  val keyName = fr._key.toString
 
   /**
    * :: DeveloperApi ::
    * Implemented by subclasses to compute a given partition.
    */
   override def compute(split: Partition, context: TaskContext): Iterator[A] = {
-    if( rdd != null ) rdd.get.compute(split,context)
-    else {
-      new Iterator[A] {
-        val fr : DataFrame = DKV.get(Key.make(h2oName)).get.asInstanceOf[DataFrame]
-        val a : A = implicitly[ClassTag[A]].runtimeClass.newInstance.asInstanceOf[A]
-        val chks = fr.getChunks(split.index)
-        val nrows = chks(0).len
-        var row : Int = 0
-        def hasNext: Boolean = row < nrows
-        def next(): A = {
-          ???
+    new Iterator[A] {
+      val fr : DataFrame = DKV.get(Key.make(keyName)).get.asInstanceOf[DataFrame]
+      val a : A = implicitly[ClassTag[A]].runtimeClass.newInstance.asInstanceOf[A]
+      val chks = fr.getChunks(split.index)
+      val nrows = chks(0).len
+      var row : Int = 0
+      def hasNext: Boolean = row < nrows
+      def next(): A = {
+        ???
           //for( i <- 0 until chks.length )
           //  a.
           //a
-        }
       }
     }
   }
 
   /** Pass thru an RDD if given one, else pull from the H2O Frame */
   override protected def getPartitions: Array[Partition] = {
-    if( rdd != null ) rdd.get.partitions
-    else { // Make partitions to draw from H2O Frame
-      val num = fr.get.anyVec().nChunks()
-      val res = new Array[Partition](num)
-      for( i <- 0 until num ) res(i) = new Partition { val index = i }
-      res
-    }
+    val num = fr.anyVec().nChunks()
+    val res = new Array[Partition](num)
+    for( i <- 0 until num ) res(i) = new Partition { val index = i }
+    res
   }
 
 }
