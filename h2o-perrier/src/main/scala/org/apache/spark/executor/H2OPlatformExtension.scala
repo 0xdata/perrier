698f2541ca54ef067e5d9b4d4ef8e47899a28e92
mmalohlava <michal.malohlava@gmail.com>
2014-09-11 17:32:00 -0700

    Missing cloud barrier.

--- h2o-perrier/src/main/scala/org/apache/spark/executor/H2OPlatformExtension.scala
+++ h2o-perrier/src/main/scala/org/apache/spark/executor/H2OPlatformExtension.scala
@@ -28,6 +28,8 @@ class H2OPlatformExtension extends PlatformExtension with Logging {
     logDebug("Starting H2O Spark Extension...")
     water.H2O.main(new Array[String](0))
     water.H2O.finalizeRequest()
+    // FIXME we can continue only if all H2O nodes are ready
+    water.H2O.waitForCloudSize(conf.getInt("spark.h2o.cluster.size", 1), 10000) // FIXME this is huge hack
     logDebug("H2O extension started.")
   }
 
